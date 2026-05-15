991e3ac0edf93273869376bf8ac0ca19f5325d87
Echo <echorw927@gmail.com>
2026-05-14 22:00:10 -0700

    t

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+cd "$(dirname"$0")"
 VENV_DIR=".venv"
+if [[ ! -t 0 ]]; then
+    gnome-terminal -- zsh -c "$0; read" || \
+    konsole -e zsh -c "$0; read" || \
+    xterm -e zsh -c "$0; read"
+    exit
+fi
+
 if [ ! -d "$VENV_DIR" ]; then
     echo "Creating virtual environment..."
     python3 -m venv "$VENV_DIR"
